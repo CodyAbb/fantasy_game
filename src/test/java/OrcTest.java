2ccfa8e1c618d52358ed3421488618977a0b953e
Gary Clark <gclark04432@live.co.uk>
2020-03-18 16:04:45 +0000

    add test for orc health

--- src/test/java/OrcTest.java
+++ src/test/java/OrcTest.java
@@ -1,40 +1,46 @@
-//import Characters.Orc;
-//import Items.IAffectHealthPoints;
-//import Items.Weapons.Weapon;
-//import org.junit.Before;
-//import org.junit.Test;
+import Characters.Orc;
+import Items.IAffectHealthPoints;
+import Items.Weapons.Weapon;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class OrcTest {
+
+   private Orc orc;
+   private IAffectHealthPoints sword = new Weapon("sword", 20);
+   private IProtect shield = new Armour("Shield", 15);
+
+
+   @Before
+   public void before(){
+       orc = new Orc("Orc", 100, sword, );
+   }
+
+   @Test
+   public void checkOrcHas100Health(){
+     assertEquals(100, orc.getHealthPoints())
+   }
 //
-//import static junit.framework.TestCase.assertEquals;
-//
-//public class OrcTest {
-//
-//    private Orc orc;
-//    private IAffectHealthPoints sword = new Weapon("sword", 20);
-//
-//
-//    @Before
-//    public void before(){
-//        orc = new Orc();
+//    @Test
+//    public void checkTreasureStartsEmpty(){
+//        assertEquals(0, orc.getTreasure().size());
 //    }
 //
-////    @Test
-////    public void checkTreasureStartsEmpty(){
-////        assertEquals(0, orc.getTreasure().size());
-////    }
+//    @Test
+//    public void checkInventoryStartsEmpty(){
+//        assertEquals(0, orc.getInventory().size());
+//    }
 //
-////    @Test
-////    public void checkInventoryStartsEmpty(){
-////        assertEquals(0, orc.getInventory().size());
-////    }
-////
-////    @Test
-////    public void checkOrcCanHaveSword(){
-////        orc.addToInventory(sword);
-////        assertEquals(1, orc.getInventory().size());
-////    }
-////
-////    @Test
-////    public void canAffectOrcHealthWithSword(){
-////        assertEquals(90, );
-////    }
-////}
+//    @Test
+//    public void checkOrcCanHaveSword(){
+//        orc.addToInventory(sword);
+//        assertEquals(1, orc.getInventory().size());
+//    }
+//
+//    @Test
+//    public void canAffectOrcHealthWithSword(){
+//        assertEquals(90, );
+//    }
+// }
